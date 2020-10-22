9b8deb110b6f66bdca0eb5d09504226d7005669c
CampoMilan <s108570@ap.be>
2020-10-22 21:19:21 +0200

    Hero moves on his own

--- Platformer/Platformer/Hero.cs
+++ Platformer/Platformer/Hero.cs
@@ -11,9 +11,12 @@ namespace Platformer
     class Hero : IGameObject
     {
         Texture2D heroTexture;
-
         Animation animation;
-        
+
+        private Vector2 position;
+        private Vector2 speed;
+        private Vector2 velocity;
+
 
         public Hero(Texture2D texture)
         {
@@ -28,16 +31,51 @@ namespace Platformer
             animation.AddFrame(new AnimationFrame(new Rectangle(503, 0, 40, 85)));
             animation.AddFrame(new AnimationFrame(new Rectangle(583, 0, 40, 85)));
             animation.AddFrame(new AnimationFrame(new Rectangle(663, 0, 40, 85)));
+
+            position = new Vector2(10, 10);
+            speed = new Vector2(1, 1);
+            velocity = new Vector2(0.1f, 0.1f);
+        }
+
+        private void Move()
+        {
+            position += speed;
+            speed += velocity;
+            speed = Limit(speed,5); //limits the vector length of speed to 5
+            
+            //invert the movement when boundaries are hit
+            if (position.X > 600 || position.X < 0)
+            {
+                speed.X *= -1;
+                velocity.X *= -1;
+            }
+            if (position.Y > 400 || position.Y < 0)
+            {
+                speed.Y *= -1;
+                velocity.Y *= -1;
+            }
+        }
+
+        private Vector2 Limit(Vector2 v, float max)
+        {
+            if (v.Length() > max)
+            {
+                var ratio = max / v.Length();
+                v.X *= ratio;
+                v.Y *= ratio;
+            }
+            return v;
         }
 
         public void Update(GameTime gameTime)
         {
+            Move();
             animation.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(heroTexture, new Vector2(10, 10), animation.CurrentFrame.SourceRectangle, Color.CornflowerBlue);
+            spriteBatch.Draw(heroTexture, position, animation.CurrentFrame.SourceRectangle, Color.CornflowerBlue);
 
         }
     }
